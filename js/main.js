9ceb531741011dc946be436e5a42eab981348e53
parkerprecisionweb <dallasjamesparker@gmail.com>
2018-08-24 15:58:10 -0600

    Images added, integrated, and JS started
    - Operator Images added
    - "0" added
    - Operate Images integrated into front end
    - JS skeleton + psuedo code for each numeric button click started

---
+++ js/main.js
@@ -0,0 +1,84 @@
+$(document).ready( function() {
+  var num1String = ""
+  var sign = ""
+  var num2String = ""
+  var num1 = nil;
+  var num2 = nil;
+
+  var display = "";
+
+  // putting event listeners on each button
+  
+  $('#button_1').click(function() {
+    // if sign is empty, concatonate "1" to the end of num1String
+    // else, concatonate "1" to the end of num2String
+    //
+    // replace contents of #display with contents of var display
+  });
+
+  $('#button_2').click(function() {
+    // if sign is empty, concatonate "2" to the end of num1String
+    // else, concatonate "2" to the end of num2String
+  });
+
+  $('#button_3').click(function() {
+    // if sign is empty, concatonate "3" to the end of num1String
+    // else, concatonate "3" to the end of num2String
+  });
+
+  $('#button_4').click(function() {
+    // if sign is empty, concatonate "4" to the end of num1String
+    // else, concatonate "4" to the end of num2String
+  });
+
+  $('#button_5').click(function() {
+    // if sign is empty, concatonate "5" to the end of num1String
+    // else, concatonate "5" to the end of num2String
+  });
+
+  $('#button_6').click(function() {
+    // if sign is empty, concatonate "6" to the end of num1String
+    // else, concatonate "6" to the end of num2String
+  });
+
+  $('#button_7').click(function() {
+    // if sign is empty, concatonate "7" to the end of num1String
+    // else, concatonate "7" to the end of num2String
+  });
+
+  $('#button_8').click(function() {
+    // if sign is empty, concatonate "8" to the end of num1String
+    // else, concatonate "8" to the end of num2String
+  });
+
+  $('#button_9').click(function() {
+    // if sign is empty, concatonate "9" to the end of num1String
+    // else, concatonate "9" to the end of num2String
+  });
+
+  $('#button_0').click(function() {
+    // if sign is empty, concatonate "0" to the end of num1String
+    // else, concatonate "0" to the end of num2String
+  });
+
+  $('#plus').click(function() {
+    // if num1 is empty, generate error message telling user to pick a number first
+    // else 
+  });
+
+  $('#minus').click(function() {
+  });
+
+  $('multiply').click(function() {
+  });
+
+  $('#devide').click(function() {
+  });
+
+  $('#square_root').click(function() {
+  });
+
+  $('#equals').click(function() {
+  });
+
+});
