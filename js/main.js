604e2e40181f88546903e12567f98491386f204c
parkerprecisionweb <dallasjamesparker@gmail.com>
2018-08-24 17:39:06 -0600

    Finished First Draft of completed Calc (Not tested)

--- js/main.js
+++ js/main.js
@@ -38,19 +38,20 @@ $(document).ready( function() {
     return result;
   }
 
-  function squared(num1, num2){
+  function squared(num1){
     result = Math.sqrt(num1)
-    // set var display = var result
-    //
+    display = result
+
     // replace contents of #display with contents of var display
-    //
-    // var num1String = ""
-    // var sign = ""
-    // var num2String = ""
-    // var num1 = nil;
-    // var num2 = nil;
-    // var result = nil; 
-    //
+    $( "#display" ).replaceWith( "<h2 class='row text-center ' id='display'>" + display + "</h2>");
+
+    // set variables back to initial state, for new operation
+    num1String = ""
+    sign = ""
+    num2String = ""
+    num1 = nil;
+    num2 = nil;
+    result = nil; 
   }
 
 /**************************************************JQuery**********************************************************/
@@ -59,139 +60,227 @@ $(document).ready( function() {
   $('#button_1').click(function() {
     // IF sign is empty, concatonate "1" to the end of num1String
     // ELSE, concatonate "1" to the end of num2String
-    //
-    // refreshDisplay()
-    //
+    if (sign == "") {
+      num1String += "1 ";
+    } else {
+      num2String += "1 ";
+    }
+
+    refreshDisplay();
+
     // replace contents of #display with contents of var display
+    $( "#display" ).replaceWith( "<h2 class='row text-center ' id='display'>" + display + "</h2>");
   });
 
   $('#button_2').click(function() {
     // IF sign is empty, concatonate "2" to the end of num1String
     // ELSE, concatonate "2" to the end of num2String
-    //
-    // refreshDisplay()
-    //
+    if (sign == "") {
+      num1String += "2 ";
+    } else {
+      num2String += "2 ";
+    }
+
+    refreshDisplay();
+
     // replace contents of #display with contents of var display
+    $( "#display" ).replaceWith( "<h2 class='row text-center ' id='display'>" + display + "</h2>");
   });
 
   $('#button_3').click(function() {
     // IF sign is empty, concatonate "3" to the end of num1String
     // ELSE, concatonate "3" to the end of num2String
-    //
-    // refreshDisplay()
-    //
+    if (sign == "") {
+      num1String += "3 ";
+    } else {
+      num2String += "3 ";
+    }
+
+    refreshDisplay();
+
     // replace contents of #display with contents of var display
+    $( "#display" ).replaceWith( "<h2 class='row text-center ' id='display'>" + display + "</h2>");
   });
 
   $('#button_4').click(function() {
     // IF sign is empty, concatonate "4" to the end of num1String
     // ELSE, concatonate "4" to the end of num2String
-    //
-    // refreshDisplay()
-    //
+    if (sign == "") {
+      num1String += "4 ";
+    } else {
+      num2String += "4 ";
+    }
+
+    refreshDisplay();
+
     // replace contents of #display with contents of var display
+    $( "#display" ).replaceWith( "<h2 class='row text-center ' id='display'>" + display + "</h2>");
   });
 
   $('#button_5').click(function() {
     // IF sign is empty, concatonate "5" to the end of num1String
     // ELSE, concatonate "5" to the end of num2String
-    //
-    // refreshDisplay()
-    //
+    if (sign == "") {
+      num1String += "5 ";
+    } else {
+      num2String += "5 ";
+    }
+
+    refreshDisplay();
+
     // replace contents of #display with contents of var display
+    $( "#display" ).replaceWith( "<h2 class='row text-center ' id='display'>" + display + "</h2>");
   });
 
   $('#button_6').click(function() {
     // IF sign is empty, concatonate "6" to the end of num1String
     // ELSE, concatonate "6" to the end of num2String
-    //
-    // refreshDisplay()
-    //
+    if (sign == "") {
+      num1String += "6 ";
+    } else {
+      num2String += "6 ";
+    }
+
+    refreshDisplay();
+
     // replace contents of #display with contents of var display
+    $( "#display" ).replaceWith( "<h2 class='row text-center ' id='display'>" + display + "</h2>");
   });
 
   $('#button_7').click(function() {
     // IF sign is empty, concatonate "7" to the end of num1String
     // ELSE, concatonate "7" to the end of num2String
-    //
-    // refreshDisplay()
-    //
+    if (sign == "") {
+      num1String += "7 ";
+    } else {
+      num2String += "7 ";
+    }
+
+    refreshDisplay();
+
     // replace contents of #display with contents of var display
+    $( "#display" ).replaceWith( "<h2 class='row text-center ' id='display'>" + display + "</h2>");
   });
 
   $('#button_8').click(function() {
     // IF sign is empty, concatonate "8" to the end of num1String
     // ELSE, concatonate "8" to the end of num2String
-    //
-    // refreshDisplay()
-    //
+    if (sign == "") {
+      num1String += "8 ";
+    } else {
+      num2String += "8 ";
+    }
+
+    refreshDisplay();
+
     // replace contents of #display with contents of var display
+    $( "#display" ).replaceWith( "<h2 class='row text-center ' id='display'>" + display + "</h2>");
   });
 
   $('#button_9').click(function() {
     // IF sign is empty, concatonate "9" to the end of num1String
     // ELSE, concatonate "9" to the end of num2String
-    //
-    // refreshDisplay()
-    //
+    if (sign == "") {
+      num1String += "9 ";
+    } else {
+      num2String += "9 ";
+    }
+
+    refreshDisplay();
+
     // replace contents of #display with contents of var display
+    $( "#display" ).replaceWith( "<h2 class='row text-center ' id='display'>" + display + "</h2>");
   });
 
   $('#button_0').click(function() {
     // IF sign is empty, concatonate "0" to the end of num1String
     // ELSE, concatonate "0" to the end of num2String
-    //
-    // refreshDisplay()
-    //
+ if (sign == "") {
+      num1String += "0 ";
+    } else {
+      num2String += "0 ";
+    }
+
+    refreshDisplay();
+
     // replace contents of #display with contents of var display
+    $( "#display" ).replaceWith( "<h2 class='row text-center ' id='display'>" + display + "</h2>");
   });
 
   $('#plus').click(function() {
     // IF num1 is empty, generate error message telling user either to pick a number first,
-    // or ELSE IF var sign is NOT empty, tell user to they can only pick one sign at a time
     // ELSE, concatonate the "+" symbol to the end of var sign
-    //
-    // refreshDisplay()
-    //
+    if (num1 == "") {
+      //some error message
+    } else {
+      sign = " +";
+    }
+
+    refreshDisplay();
+
     // replace contents of #display with contents of var display
+      
+    $( "#display" ).replaceWith( "<h2 class='row text-center ' id='display'>" + display + "</h2>");
   });
 
   $('#minus').click(function() {
     // IF num1 is empty, generate error message telling user either to pick a number first,
-    // or ELSE IF var sign is NOT empty, tell user to they can only pick one sign at a time
     // ELSE, concatonate the "-" symbol to the end of var sign
-    //
-    // refreshDisplay()
-    //
+    if (num1 == "") {
+      //some error message
+    } else {
+      sign = " -";
+    }
+
+    refreshDisplay();
+
     // replace contents of #display with contents of var display
+      
+    $( "#display" ).replaceWith( "<h2 class='row text-center ' id='display'>" + display + "</h2>");
   });
 
   $('multiply').click(function() {
     // IF num1 is empty, generate error message telling user either to pick a number first,
-    // or ELSE IF var sign is NOT empty, tell user to they can only pick one sign at a time
     // ELSE, concatonate the "x" symbol to the end of var sign
-    //
-    // refreshDisplay()
-    //
+    if (num1 == "") {
+      //some error message
+    } else {
+      sign = " x";
+    }
+
+    refreshDisplay();
+
     // replace contents of #display with contents of var display
+      
+    $( "#display" ).replaceWith( "<h2 class='row text-center ' id='display'>" + display + "</h2>");
   });
 
   $('#divide').click(function() {
     // IF num1 is empty, generate error message telling user either to pick a number first,
-    // or ELSE IF var sign is NOT empty, tell user to they can only pick one sign at a time
     // ELSE, concatonate the "/" symbol to the end of var sign
-    //
-    // refreshDisplay()
-    //
+    if (num1 == "") {
+      //some error message
+    } else {
+      sign = " /";
+    }
+
+    refreshDisplay();
+
     // replace contents of #display with contents of var display
+      
+    $( "#display" ).replaceWith( "<h2 class='row text-center ' id='display'>" + display + "</h2>");
   });
 
   $('#square_root').click(function() {
     // IF num1 is empty, generate error message telling user to pick a number first,
-    // or ELSE IF var sign is NOT empty, tell user to they can only pick one sign at a time
-    // ELSE, concatonate the "&#8730;" symbol  code to the end of var sign
-    //
-    // ELSE set var display = squared(num1, num2)
+    // ELSE, set  var display = squared(num1)
+    
+    if (num1 == "") {
+      //some error message
+    } else {
+      display = squared(num1);
+    }
+
   });
 
   $('#equals').click(function() {
@@ -202,17 +291,32 @@ $(document).ready( function() {
     // ELSE IF var sign == "-" set var display = subtract(num1, num2)
     // ELSE IF var sign == "x" set var display = multiply(num1, num2)
     // ELSE IF var sign == "/" set var display = divide(num1, num2)
+    if (num1 = "") {
+      // some error message
+    } else if (sign == "") {
+      // some error message
+    } else if (num2 == "") {
+      // some error message
+    } else if (sign == "+") {
+      // some error message
+    } else if (sign == "-") {
+      // some error message
+    } else if (sign == "x") {
+      // some error message
+    } else if (sign == "/") {
+      // some error message
+    } else {
+      // some error message
+    }
     
-    //
-    // replace contents of #display with contents of var display
-    //
-    // var num1String = ""
-    // var sign = ""
-    // var num2String = ""
-    // var num1 = nil;
-    // var num2 = nil;
-    // var result = nil; 
-    //
+    $( "#display" ).replaceWith( "<h2 class='row text-center ' id='display'>" + display + "</h2>");
+
+    var num1String = ""
+    var sign = ""
+    var num2String = ""
+    var num1 = nil;
+    var num2 = nil;
+    var result = nil; 
   });
 
 });
